93f41d68f5da301c49b9b2f5f4168daa0f0bc86a
zerothehero555 <73942835+zerothehero555@users.noreply.github.com>
2021-01-05 18:52:13 +0530

    Update slingshot.js

--- slingshot.js
+++ slingshot.js
@@ -8,6 +8,9 @@ class Slingshot{
          fly(){
               this.sling.bodyA =null; 
            } 
+           attach(body){
+            this.sling.bodyA = body;               
+           }
            display(){ 
                if(this.sling.bodyA){ 
                    var pointA = this.sling.bodyA.position; 
